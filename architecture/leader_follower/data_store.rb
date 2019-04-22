0ec83d15c6bf4354ffd73f17e2581b1f7cd842aa
Americo <ame.zuz@gmail.com>
2019-04-22 00:48:45 -0700

    Create a server with only a leader

--- architecture/leader_follower/data_store.rb
+++ architecture/leader_follower/data_store.rb
@@ -1,26 +1,45 @@
 # from http://blog.self-loop.com/architecture_lecture/html/17_leader_demo.html
 
-require "thread"
-
 class UnknownCommandException < Exception; end
 class ReadOnlyException < Exception; end
 
 class DataStore
   def initialize
+    @data = Hash.new(0)
   end
 
   def increment(key)
+    @data[key] += 1
   end
 
   def square(key)
+    @data[key] *= @data[key]
   end
 
   def get(key)
+    @data[key]
   end
 
   def perform_command(command, key, read_only:)
+    case command
+    when "GET"
+      get(key)
+    when "INCREMENT"
+      raise ReadOnlyException if read_only
+
+      increment(key)
+      "OK"
+    when "SQUARE"
+      raise ReadOnlyException if read_only
+
+      square(key)
+      "OK"
+    else
+      raise UnknownCommandException
+    end
   end
 
   def get_log_lines(start_position)
+    # TODO: implement with followers.
   end
 end
--- architecture/leader_follower/main.rb
+++ architecture/leader_follower/main.rb
@@ -10,12 +10,12 @@ Thread::abort_on_exception = true
 def run_leader!(request_server_port, log_server_port)
   data_store = DataStore.new
   request_server = RequestServer.new(data_store, request_server_port, read_only: false)
-  log_server = LogServer.new(data_store, log_server_port)
+  # log_server = LogServer.new(data_store, log_server_port)
 
   request_server.run!
-  log_server.run!
+  # log_server.run!
   request_server.join
-  log_server.join
+  # log_server.join
 end
 
 def run_follower!(request_server_port, log_server_port)
@@ -30,7 +30,7 @@ def run_follower!(request_server_port, log_server_port)
 end
 
 def main
-  raise "[--leader|--follower] request_server_port log_server_port" unless ARGV.length = 3
+  raise "[--leader|--follower] request_server_port log_server_port" unless ARGV.length == 3
   request_server_port = Integer(ARGV[1])
   log_server_port = Integer(ARGV[2])
 
