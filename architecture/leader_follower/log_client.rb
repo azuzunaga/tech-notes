0e94907e04ee836e665a116f0d69909cde9a193e
Americo <ame.zuz@gmail.com>
2019-04-23 14:23:48 +0800

    Set up log client to transmit data to follower

--- architecture/leader_follower/log_client.rb
+++ architecture/leader_follower/log_client.rb
@@ -3,12 +3,23 @@
 require "socket"
 
 class LogClient
-  def initialize(data_store, port)
+  def initialize(data_store, log_server_port)
+    @data_store = data_store
+    @log_connection = TCPSocket.new("localhost", log_server_port)
+    @thread = nil
   end
 
   def run!
+    @thread = Thread.new do
+      loop do
+        command, key = @log_connection.gets.chomp.split
+        puts "Received Log Line: #{command} #{key}"
+        @data_store.perform_command(command, key, read_only: false)
+      end
+    end
   end
 
   def join
+    @thread.join
   end
 end
